a17dcb60ee5b1d5ed29c9229b9b457e8b96add67
kacperski1 <kacperski1@gmail.com>
2013-08-11 23:15:33 +0200

    Update kacperski1_worldedit.js

--- kacperski1_worldedit.js
+++ kacperski1_worldedit.js
@@ -1,5 +1,5 @@
 // WorldEditPE mod by kacperski1
-// version 0.3
+// version 0.4
 
 var x1 = 0;
 var y1 = 0;
@@ -7,7 +7,7 @@ var z1 = 0;
 var x2 = 0;
 var y2 = 0;
 var z2 = 0;
-var topaste;
+var clip;
 
 function wMessage(msg)
 {
@@ -20,7 +20,7 @@ function procCmd(cmd)
 	switch(Command[0])
 	{
 		case "help":
-			clientMessage("Available commands: /help, //wand, //set, //replace, //drain, /snow, /thaw, //hcuboid");
+			clientMessage("Available commands: /help, //wand, //set, //replace, //drain, /snow, /thaw, //hcuboid, //cut, //copy, //paste, /netherize, /unnetherize, /reactor, /untree");
 			break;
 		
 		case "/wand":
@@ -193,6 +193,203 @@ function procCmd(cmd)
 			wMessage("Successfully created "+BlockNum+" blocks!");
 			break;
 			
+		case "/copy":
+			var lowestX = Math.min(x1, x2);
+			var lowestY = Math.min(y1, y2);
+			var lowestZ = Math.min(z1, z2);
+			var highestX = Math.max(x1, x2);
+			var highestY = Math.max(y1, y2);
+			var highestZ = Math.max(z1, z2);
+			var wX = (highestX - lowestX) + 1;
+			var wY = (highestY - lowestY) + 1;
+			var wZ = (highestZ - lowestZ) + 1;
+			var BlockNum = 0;
+			
+			clip = new Array(wX);
+			for (var x = 0; x < wX; ++x) 
+			{
+				clip[x] = new Array(wY);
+				for (var y = 0; y < wY; ++y) 
+				{
+					clip[x][y] = new Array(wZ);
+					for (var z = 0; z < wZ; ++z) 
+					{
+						clip[x][y][z] = getTile(lowestX + x, lowestY + y, lowestZ + z);
+						BlockNum++;
+					}
+				}
+			}
+			wMessage("Copied "+BlockNum+" blocks to clipboard");
+			break;
+			
+		case "/paste":
+			var BlockNum = 0;
+			for (var x = 0; x < clip.length; ++x) 
+			{
+				for (var y = 0; y < clip[0].length; ++y) 
+				{
+					for (var z = 0; z < clip[0][0].length; ++z) 
+					{
+						setTile(x1+x, y1+y, z1+z, clip[x][y][z]);
+						BlockNum++;
+					}
+				}
+			}
+			wMessage("Successfully pasted "+BlockNum+" blocks!");
+			break;
+			
+		case "/cut":
+			var lowestX = Math.min(x1, x2);
+			var lowestY = Math.min(y1, y2);
+			var lowestZ = Math.min(z1, z2);
+			var highestX = Math.max(x1, x2);
+			var highestY = Math.max(y1, y2);
+			var highestZ = Math.max(z1, z2);
+			var wX = (highestX - lowestX) + 1;
+			var wY = (highestY - lowestY) + 1;
+			var wZ = (highestZ - lowestZ) + 1;
+			var BlockNum = 0;
+			
+			clip = new Array(wX);
+			for (var x = 0; x < wX; ++x) 
+			{
+				clip[x] = new Array(wY);
+				for (var y = 0; y < wY; ++y) 
+				{
+					clip[x][y] = new Array(wZ);
+					for (var z = 0; z < wZ; ++z) 
+					{
+						clip[x][y][z] = getTile(lowestX + x, lowestY + y, lowestZ + z);
+						setTile(lowestX + x, lowestY + y, lowestZ + z, 0);
+						BlockNum++;
+					}
+				}
+			}
+			wMessage("Moved "+BlockNum+" blocks to clipboard");
+			break;
+			
+		case "netherize":
+			var lowestX = Math.min(x1, x2);
+			var lowestY = Math.min(y1, y2);
+			var lowestZ = Math.min(z1, z2);
+			var highestX = Math.max(x1, x2);
+			var highestY = Math.max(y1, y2);
+			var highestZ = Math.max(z1, z2);
+			
+			for(var x = lowestX; x <= highestX; x++)
+			{
+				for(var y = lowestY; y <= highestY; y++)
+				{
+					for(var z = lowestZ; z <= highestZ; z++)
+					{
+						switch(getTile(x,y,z))
+						{
+							case 2: setTile(x,y,z,87);break;
+							case 3: setTile(x,y,z,87);break;
+							case 8: setTile(x,y,z,10);break;
+							case 9: setTile(x,y,z,11);break;
+							case 17: setTile(x,y,z,0);break;
+							case 18: setTile(x,y,z,89);break;
+							case 1: setTile(x,y,z,87);break;
+							case 12: setTile(x,y,z,87);break;
+							case 15: setTile(x,y,z,89);break;
+							case 37: setTile(x,y,z,40);break;
+							case 38: setTile(x,y,z,51);break;
+							case 60: setTile(x,y,z,87);break;
+							case 59: setTile(x,y,z,51);break;
+							case 109: setTile(x,y,z,114);break;
+						}
+					}
+				}
+			}
+			wMessage("Successfully Netherized!");
+			break;
+			
+		case "unnetherize":
+			var lowestX = Math.min(x1, x2);
+			var lowestY = Math.min(y1, y2);
+			var lowestZ = Math.min(z1, z2);
+			var highestX = Math.max(x1, x2);
+			var highestY = Math.max(y1, y2);
+			var highestZ = Math.max(z1, z2);
+			
+			for(var x = lowestX; x <= highestX; x++)
+			{
+				for(var y = lowestY; y <= highestY; y++)
+				{
+					for(var z = lowestZ; z <= highestZ; z++)
+					{
+						switch(getTile(x,y,z))
+						{
+							case 87: if(getTile(x,y+1,z) == 0) 
+									 {
+										setTile(x,y,z,2);
+									 } 
+									 else if(getTile(x,y+5,z) == 0)
+									 {
+										setTile(x,y,z,3);
+									 } 
+									 else {setTile(x,y,z,1);}
+									 ;break;
+							case 10: setTile(x,y,z,8);break;
+							case 11: setTile(x,y,z,9);break;
+							case 89: setTile(x,y,z,18);break;
+							case 37: setTile(x,y,z,40);break;
+							case 51: setTile(x,y,z,38);break;
+							case 114: setTile(x,y,z,109);break;
+						}
+					}
+				}
+			}
+			wMessage("Successfully Unnetherized!");
+			break;
+			
+		case "reactor":
+			setTile(x,y+1,z,4);
+			setTile(x-1,y+1,z,4);
+			setTile(x+1,y+1,z,4);
+			setTile(x,y+1,z-1,4);
+			setTile(x,y+1,z+1,4);
+			setTile(x-1,y+1,z-1,41);
+			setTile(x-1,y+1,z+1,41);
+			setTile(x+1,y+1,z-1,41);
+			setTile(x+1,y+1,z+1,41);
+			setTile(x,y+2,z,247);
+			setTile(x-1,y+2,z-1,4);
+			setTile(x-1,y+2,z+1,4);
+			setTile(x+1,y+2,z-1,4);
+			setTile(x+1,y+2,z+1,4);
+			setTile(x,y+3,z,4);
+			setTile(x-1,y+3,z,4);
+			setTile(x+1,y+3,z,4);
+			setTile(x,y+3,z-1,4);
+			setTile(x,y+3,z+1,4);
+			wMessage("Nether Reactor created successfully!");
+			break;
+			
+		case "untree":
+			var lowestX = Math.min(x1, x2);
+			var lowestY = Math.min(y1, y2);
+			var lowestZ = Math.min(z1, z2);
+			var highestX = Math.max(x1, x2);
+			var highestY = Math.max(y1, y2);
+			var highestZ = Math.max(z1, z2);
+			
+			for(var x = lowestX; x <= highestX; x++)
+			{
+				for(var y = lowestY; y <= highestY; y++)
+				{
+					for(var z = lowestZ; z <= highestZ; z++)
+					{
+						if(getTile(x,y,z) == 17 || getTile(x,y,z) == 18 || getTile(x,y,z) == 37 || getTile(x,y,z) == 38 || getTile(x,y,z) == 39 || getTile(x,y,z) == 40)
+						{
+							setTile(x,y,z,0);
+						}
+					}
+				}
+			}
+			wMessage("Successfully... untreed? :P");
+			break;
 			
 	}
 }
