9d5e64480ef19b0a61f73f1f7c9b06910b421861
kacperski1 <kacperski1@gmail.com>
2013-08-13 21:07:31 +0200

    Create kacperski1_caves.js

---
+++ kacperski1_caves.js
@@ -0,0 +1,49 @@
+// Caves mod by kacperski1
+// version 0.1
+
+function generateCaves()
+{
+	var cX;
+	var cY;
+	var cZ;
+	var smaY = 129;
+	
+	clientMessage("[Caves] Loading data...");
+	for(int xx; xx < 256; xx++)
+	{
+		for(int zz; zz < 256; zz++)
+		{
+			for(int yy; yy < 128; yy++)
+			{
+				if(getTile(xx,yy,zz) == 2)
+				{
+					if(yy < smaY) {smaY = yy;}
+				}
+			}
+		}
+	}
+	clientMessage("[Caves] Generating caves...");
+	for(int xx; xx < 256; xx++)
+	{
+		for(int zz; zz < 256; zz++)
+		{
+			for(int yy; yy < smaY; yy++)
+			{
+				switch(getTile(xx,yy,zz))
+				{
+					case 3: setTile(xx,yy,zz,0);
+					case 13: setTile(xx,yy,zz,0);
+				}
+			}
+		}
+	}
+	clientMessage("[Caves] Caves generated! You can now disable this script!");
+}
+
+function useItem(x,y,z,itemId,blockId)
+{
+	if(itemId == 280)
+	{
+		generateCaves();
+	}
+}
