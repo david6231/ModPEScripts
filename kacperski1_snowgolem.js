7aea7ce2ef04511c51e6c6d76b602084f4ea1ff5
kacperski1 <kacperski1@gmail.com>
2013-08-16 19:42:15 +0200

    Update kacperski1_snowgolem.js

--- kacperski1_snowgolem.js
+++ kacperski1_snowgolem.js
@@ -1,20 +1,17 @@
 // Snow Golem mod by kacperski1
-// version 0.2
+// version 0.3
 
 var gX;
 var gY;
 var gZ;
-var preX;
-var preY;
-var preZ;
 var direction;
 
 var active = 0;
 var tickCurrent = 0;
-var tickDuration = 30;
+var tickDuration = 15;
 var debugActive = 0;
 
-var activationItem = 332;
+var activationItem = 362;
 var actBlock1 = 103;
 var actBlock2 = 80;
 var actBlock3 = 80;
@@ -28,21 +25,15 @@ function useItem(x,y,z,itemId,blockId)
 			gX = x;
 			gY = y - 2;
 			gZ = z;
-			preX = x;
-			preY = y - 2;
-			preZ = z;
 			direction = Math.floor((Math.random()*4)+1);
 			active = 1;
 		}
 
-		if(blockId == actBlock2 && getTile(x,y+1,z) == actBlock1 && getTile(x,y-2,z) == actBlock3)
+		if(blockId == actBlock2 && getTile(x,y+1,z) == actBlock1 && getTile(x,y-1,z) == actBlock3)
 		{
 			gX = x;
 			gY = y - 1;
 			gZ = z;
-			preX = x;
-			preY = y - 1;
-			preZ = z;
 			direction = Math.floor((Math.random()*4)+1);
 			active = 1;
 		}
@@ -52,9 +43,6 @@ function useItem(x,y,z,itemId,blockId)
 			gX = x;
 			gY = y;
 			gZ = z;
-			preX = x;
-			preY = y;
-			preZ = z;
 			direction = Math.floor((Math.random()*4)+1);
 			active = 1;
 		}
@@ -87,6 +75,66 @@ function checkExistence()
 	return 0;
 }
 
+function canMoveOnFloor(px,py,pz)
+{
+	switch(getTile(px,py,pz))
+	{
+		case 0:
+		case 6:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+		case 26:
+		case 30:
+		case 31:
+		case 32:
+		case 37:
+		case 38:
+		case 39:
+		case 40:
+		case 44:
+		case 50:
+		case 51:
+		case 53:
+		case 54:
+		case 59:
+		case 63:
+		case 64:
+		case 65:
+		case 67:
+		case 68:
+		case 71:
+		case 78:
+		return 0; break;
+		
+		return 1;
+	}
+}
+
+function canMoveOn(ox,oy,oz)
+{
+	if((getTile(ox,oy,oz) == 0 || getTile(ox,oy,oz) == 78) && getTile(ox,oy+1,oz) == 0 && getTile(ox,oy+2,oz) == 0)
+	{
+		if(canMoveOnFloor(ox,oy-1,oz))
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+function MoveOn(mx,my,mz)
+{
+	setTile(gX,gY,gZ,78);
+	setTile(gX,gY+1,gZ,0);
+	setTile(gX,gY+2,gZ,0);
+	
+	setTile(mx,my,mz,actBlock3);
+	setTile(mx,my+1,mz,actBlock2);
+	setTile(mx,my+2,mz,actBlock1);
+}
+
 function modTick()
 {
 	if(active == 1)
@@ -102,64 +150,36 @@ function modTick()
 			switch(direction)
 			{
 				case 1:
-					if((getTile(gX+1,gY,gZ) == 0 || getTile(gX+1,gY,gZ) == 78) && getTile(gX+1,gY+1,gZ) == 0 && getTile(gX+1,gY+2,gZ) == 0)
+					if(canMoveOn(gX+1,gY,gZ))
 					{
-						setTile(gX,gY,gZ,78);
-						setTile(gX,gY+1,gZ,0);
-						setTile(gX,gY+2,gZ,0);
-						
-						setTile(gX+1,gY,gZ,actBlock3);
-						setTile(gX+1,gY+1,gZ,actBlock2);
-						setTile(gX+1,gY+2,gZ,actBlock1);
-						
+						MoveOn(gX+1,gY,gZ);
 						gX = gX+1;
 					}
 					else {direction = Math.floor((Math.random()*4)+1);}
 					break;
 					
 				case 2:
-					if((getTile(gX-1,gY,gZ) == 0 || getTile(gX-1,gY,gZ) == 78) && getTile(gX-1,gY+1,gZ) == 0 && getTile(gX-1,gY+2,gZ) == 0)
+					if(canMoveOn(gX-1,gY,gZ))
 					{
-						setTile(gX,gY,gZ,78);
-						setTile(gX,gY+1,gZ,0);
-						setTile(gX,gY+2,gZ,0);
-						
-						setTile(gX-1,gY,gZ,actBlock3);
-						setTile(gX-1,gY+1,gZ,actBlock2);
-						setTile(gX-1,gY+2,gZ,actBlock1);
-						
+						MoveOn(gX-1,gY,gZ);
 						gX = gX-1;
 					}
 					else {direction = Math.floor((Math.random()*4)+1);}
 					break;
 
 				case 3:
-					if((getTile(gX,gY,gZ+1) == 0 || getTile(gX,gY,gZ+1) == 78) && getTile(gX,gY+1,gZ+1) == 0 && getTile(gX,gY+2,gZ+1) == 0)
+					if(canMoveOn(gX,gY,gZ+1))
 					{
-						setTile(gX,gY,gZ,78);
-						setTile(gX,gY+1,gZ,0);
-						setTile(gX,gY+2,gZ,0);
-						
-						setTile(gX,gY,gZ+1,actBlock3);
-						setTile(gX,gY+1,gZ+1,actBlock2);
-						setTile(gX,gY+2,gZ+1,actBlock1);
-						
+						MoveOn(gX,gY,gZ+1);
 						gZ = gZ+1;
 					}
 					else {direction = Math.floor((Math.random()*4)+1);}
 					break;
 					
 				case 4:
-					if((getTile(gX,gY,gZ-1) == 0 || getTile(gX,gY,gZ-1) == 78) && getTile(gX,gY+1,gZ-1) == 0 && getTile(gX,gY+2,gZ-1) == 0)
+					if(canMoveOn(gX,gY,gZ-1))
 					{
-						setTile(gX,gY,gZ,78);
-						setTile(gX,gY+1,gZ,0);
-						setTile(gX,gY+2,gZ,0);
-						
-						setTile(gX,gY,gZ-1,actBlock3);
-						setTile(gX,gY+1,gZ-1,actBlock2);
-						setTile(gX,gY+2,gZ-1,actBlock1);
-						
+						MoveOn(gX,gY,gZ-1);
 						gZ = gZ-1;
 					}
 					else {direction = Math.floor((Math.random()*4)+1);}
@@ -167,15 +187,6 @@ function modTick()
 			}
 			
 			
-		}
-	
-		if(getTile(gX,gY-1,gZ) == 0)
-		{
-			setTile(gX,gY+2,gZ,0);
-			setTile(gX,gY+1,gZ,actBlock1);
-			setTile(gX,gY,gZ,actBlock2);
-			setTile(gX,gY-1,gZ,actBlock3);
-			gY--;
 		}
 	}
 }
