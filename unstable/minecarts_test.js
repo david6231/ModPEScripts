f5e1aedaf45160804ccdc5e1dc7ff5822556e97f
kacperski1 <kacperski1@gmail.com>
2013-11-20 19:22:05 +0100

    Update minecarts_test.js

--- unstable/minecarts_test.js
+++ unstable/minecarts_test.js
@@ -1,17 +1,5 @@
 // Minecart mod by kacperski1
-// version 0.0.3 UNSTABLE
-
-var buttonWindow = null;
-
-function leaveGame() {
-  var activity = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
-  activity.runOnUiThread(new java.lang.Runnable({ run: function() {
-        if(buttonWindow != null) {
-          buttonWindow.dismiss();
-          buttonwindow = null;
-        }
-  }}));
-}
+// version 0.0.2
 
 var init = false;
 
@@ -215,30 +203,3 @@ function modTick()
 		}
 	}
 }
-
-function  showButton() {
-  var activity = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();    
-  activity.runOnUiThread(new java.lang.Runnable({ run: function() {
-        try{
-          buttonWindow = new android.widget.PopupWindow();
-          var layout = new android.widget.RelativeLayout(activity);
-          var button = new android.widget.Button(activity);
-          button.setText("Leave");
-          button.setOnClickListener(new android.view.View.OnClickListener({
-                onClick: function(viewarg) {
-				riding = 0;
-                }
-          }));
-          layout.addView(button);
-          buttonWindow.setContentView(layout);
-          buttonWindow.setWidth(android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
-          buttonWindow.setHeight(android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
-          buttonWindow.setBackgroundDrawable(new
-android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
-          buttonWindow.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.BOTTOM, 0, 0);
-        }catch(problem){
-          print("Error: User can't dance. Or the button can't be drawn, I don't know yet.");
-        }
-  }}));
-}
-	try { buttonWindow.show
